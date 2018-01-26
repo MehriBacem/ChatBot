83f60de1be02672cc8e8c525911aa6989c66ceb7
MehriBacem <33914501+MehriBacem@users.noreply.github.com>
2018-01-26 01:15:12 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,125 +1,171 @@
-service: lambda-function
+'use strict';
+var VERIFY_TOKEN = "bacem";
+var https = require('https');
+var mysql= require('mysql');
+var PAGE_ACCESS_TOKEN = "EAAHXCZCScr1MBADvIcE4o7beSmkSmtqdk3DXz7S8RsQ5BFMZC0rLSLsJS9UFFXe5XT1RSpwzrn0LySNaPSx7TQxq6biCTAtkBSb3Wm9SYotNCd6cHkGcJCGlp3GcVYvxWq6NchoqoBnfbNjkUcpAjSWJTKoHaY3IU6AsIBAqtVsHquxwP5";
+module.exports.ChatBot = (event, context, callback) => {
 
-provider:
-  name: aws
-  runtime: nodejs6.10
-  region: 'us-east-1'
-  vpc:
-    securityGroupIds:
-      - "Fn::GetAtt": ServerlessSecurityGroup.GroupId
-    subnetIds:
-      - Ref: ServerlessPublicSubnetA
-      - Ref: ServerlessPublicSubnetB
-  iamRoleStatements:
-    - Effect: Allow
-      Action:
-        - ec2:CreateNetworkInterface
-        - ec2:DescribeNetworkInterfaces
-        - ec2:DeleteNetworkInterface
-      Resource: "*"
-functions:
-  ChatBot:
-    handler: handler.ChatBot
-    events:
-     - http:
-         path: /webhook
-         method: GET   
-         integration: lambda
+  // process GET request to check our endpoint during configuration.
+  if(event.method === 'GET'){
 
-     - http:
-         path: /webhook
-         method: POST   
-         integration: lambda
-  DB:
-    handler: handler.DB
-    events:
-     - http:
-         path: /DB
-         method: ANY  
-         integration: lambda 
+    if(event.query){
 
-resources:
-  Resources:
+      var VerifyToken = event.query['hub.verify_token'] 
 
-    ServerlessVPC:
-      Type: AWS::EC2::VPC
-      Properties:
-        CidrBlock: "10.0.0.0/16"
-
-    ServerlessPublicSubnetA:
-      DependsOn: ServerlessVPC
-      Type: AWS::EC2::Subnet
-      Properties:
-        VpcId:
-          Ref: ServerlessVPC
-        AvailabilityZone: ${self:provider.region}a
-        CidrBlock: "10.0.0.0/24"
+    if (VerifyToken === VERIFY_TOKEN) {  // verify that the hub.verify_token matches the value provided when activating the Webhook. This is a security measure to authenticate the request and identify the Webhook.
+      var challenge = event.query['hub.challenge']  // a random string
+      
+      callback(null, parseInt(challenge)); // We will know that our endpoint is correctly configured and that the answer is authentic.
+    }else{
+      callback(null,'you messed up! , Wrong Token' );
+    }
 
 
-    ServerlessPublicSubnetB:
-      DependsOn: ServerlessVPC
-      Type: AWS::EC2::Subnet
-      Properties:
-        VpcId:
-          Ref: ServerlessVPC
-        AvailabilityZone: ${self:provider.region}b
-        CidrBlock: "10.0.1.0/24"
+  }
+} else{
+        // process POST request 
+        if(event.method === 'POST'){
+          var data =event.body;  
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+    // Iterate over each entry 
+    // entry : The list of modifications
 
-    ServerlessSecurityGroup:
-      DependsOn: ServerlessVPC
-      Type: AWS::EC2::SecurityGroup
-      Properties:
-        GroupDescription: SecurityGroup for Serverless Functions
-        VpcId:
-          Ref: ServerlessVPC
-    ServerlessStorageSecurityGroup:
-      DependsOn: ServerlessVPC
-      Type: AWS::EC2::SecurityGroup
-      Properties:
-        GroupDescription: Ingress for RDS Instance
-        VpcId:
-          Ref: ServerlessVPC
-        SecurityGroupIngress:
-        - IpProtocol: tcp
-          FromPort: '3306'
-          ToPort: '3306'
-          SourceSecurityGroupId:
-            Ref: ServerlessSecurityGroup
-    ServerlessRDSSubnetGroup:
-      Type: AWS::RDS::DBSubnetGroup
-      Properties:
-        DBSubnetGroupDescription: "RDS Subnet Group"
-        SubnetIds:
-        - Ref: ServerlessPublicSubnetA
-        - Ref: ServerlessPublicSubnetB
-    ServerlessRDSDBParameterGroup:
-      Type: AWS::RDS::DBParameterGroup
-      Properties: 
-        Description: parameter group
-        Family: MySQL5.7
-        Parameters:
-          character_set_database: "utf8mb4"
-          character_set_client: "utf8mb4"
-          character_set_connection: "utf8mb4"
-          character_set_results: "utf8mb4"
-          character_set_server: "utf8mb4"
-          skip-character-set-client-handshake : "TRUE"
+    data.entry.forEach(function(entry) {
+        // Iterate over each messaging event
+        entry.messaging.forEach(function(msg) {
+          if (msg.message) { 
+            
+            receivedMessage(msg,callback) ;
+          } else {
 
-    ServerlessRDSCluster:
-      DependsOn: ServerlessStorageSecurityGroup
-      Type: AWS::RDS::DBInstance
-      Properties:
-        Engine: mysql
-        EngineVersion: 5.7.19
-        DBName: Emojis
-        MasterUsername: bacem
-        MasterUserPassword: 'dictionnaire'
-        DBInstanceClass: db.t2.micro
-        AllocatedStorage: 20
-        VPCSecurityGroups:
-        - "Fn::GetAtt": ServerlessStorageSecurityGroup.GroupId
-        DBSubnetGroupName:
-          Ref: ServerlessRDSSubnetGroup
-        DBParameterGroupName:
-          Ref: ServerlessRDSDBParameterGroup
+            callback(null, "Webhook received unknown event: "+ event);
+          }
+        });
+      });
+    
+  }
+  
+}
+}
+}
+
+
+function receivedMessage(event,callback) {
+
+  var senderID = event.sender.id;
+  var message = event.message;
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+  if (messageText) {
+    // If we receive a text message, just echo the text we received.
+    if (messageText) {
+      sendTextMessage(senderID, messageText);
+      callback(null,messageText) ;
+      
+    }
+  } else if (messageAttachments) {
+        // If we receive an attachement, we respond by an url of the same  attachement.
+
+        var attachment_url = messageAttachments[0].payload.url;
+        sendTextMessage(senderID, attachment_url );
+        callback(null,attachment_url) ;
+        
+      }
+    }
+
+
+//This  function will echo the text we received.
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {   
+      id: recipientId    //The same Id of the sender. He becomes the recipient.
+    },
+    message: {
+      text: messageText
+    }
+  };
+  var body = JSON.stringify(messageData);
+ // Send our message by sending a POST request to the Send API on https://graph.facebook.com/v2.6/me/messages
+  var path = '/v2.6/me/messages?access_token=' + PAGE_ACCESS_TOKEN;  //PAGE_ACCESS_TOKEN is required to start using APIS.
+  var options = {
+    host: "graph.facebook.com",
+    path: path,
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'}
+  };
+
+  var callback = function(response) {
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+    response.on('end', function () {
+
+    });
+  }
+  // Send the HTTP request to the Messenger Platform
+  var req = https.request(options, callback);
+
+  req.on('error', function(e) {
+    console.log('problem with request: '+ e);
+  });
+
+  req.write(body);
+  req.end();
+
+  
+}
+
+
+
+  
+module.exports.DB = (event, context, callback) => {
+  
+const connection = mysql.createConnection({
+  host     : "ls118lgqhoihsh5.cjng3cg4xp6l.us-east-1.rds.amazonaws.com",
+  user     : "bacem",
+  password : "dictionnaire",
+  database : "Emojis"
+  
+})
+/* Initialization part ends here */
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const  table= 'DROP Table IF exists Persons; ';
+   connection.query(table, function (error, results, fields) {
+    if (error) {
+      return callback(error)
+    }
+    });
+  const table2=  'CREATE table Persons(a int, b varchar(64));'
+  connection.query(table2, function (error, results, fields) {
+    if (error) {
+      return callback(error)
+    }
+    });
+  const sql1 = 'insert into Persons values(5, "😐");';
+  connection.query(sql1, function (error, results, fields) {
+    if (error) {
+      return callback(error)
+    }
+    });
+
+      const sql2= 'insert into Persons values(6,"😍");';
+     connection.query(sql2, function (error, results, fields) {
+    if (error) {
+      return callback(error)
+    }
+    });
+  
+   const sql3 = 'select * from Persons;';
+  connection.query(sql3, function (error, results, fields) {
+    if (error) {
+      return callback(error)
+    }
+    callback(null,results)
+  });
+
+};
+
 
